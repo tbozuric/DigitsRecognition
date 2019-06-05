ac644d2b7534f77dafd625c78d5e3c881cabb545
Tomislav Bozuric <tomislav.bozuric@fer.hr>
2019-06-05 22:45:05 +0200

    Remove unnecessary imports

--- src/main/java/hr/fer/zemris/projekt/Engine.java
+++ src/main/java/hr/fer/zemris/projekt/Engine.java
@@ -1,34 +1,13 @@
 package hr.fer.zemris.projekt;
 
-import hr.fer.zemris.projekt.image.IImageFilter;
-import hr.fer.zemris.projekt.image.ImageTransformer;
-import hr.fer.zemris.projekt.image.binarization.OtsuBinarization;
-import hr.fer.zemris.projekt.image.border.BorderImage;
-import hr.fer.zemris.projekt.image.dilation.BinaryDilationFilter;
-import hr.fer.zemris.projekt.image.grayscale.BT709GrayscaleFilter;
-import hr.fer.zemris.projekt.image.interpolation.NearestNeighborInterpolation;
-import hr.fer.zemris.projekt.image.segmentation.ConnectedComponent;
-import hr.fer.zemris.projekt.image.translation.CentreOfMassTranslation;
-import hr.fer.zemris.projekt.image.managers.ImageManager;
 import hr.fer.zemris.projekt.neural.ConvNetClassifier;
 import hr.fer.zemris.projekt.neural.INetwork;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Engine {
 
-    private static final Path inputImagesDirectory = Paths.get("src/main/resources/outputs_transformed/9");
-    private static final Path outputDirectory = Paths.get("src/main/resources/outputs_transformed/9_transformed");
-
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         try {
             INetwork learner = new ConvNetClassifier(28, 28,
                     1, 10, 54, 30);
